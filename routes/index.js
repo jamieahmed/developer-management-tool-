b3f6274ec16c33cad570f441b02d9bd2f8491e89
Jamie Ahmed <jamieahmed273@gmail.com>
2022-07-01 11:10:53 -0400

    add styling

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const router = Router()
 router.get('/', function (req, res) {
   Comment.find({})
     .then(comments => {
-      res.render('index', { title: 'Home Page', comments: comments.slice(0,9),user: req.user ? req.user : null })
+      res.render('index', { title: 'Home Page', comments: comments.slice(0, 9), user: req.user ? req.user : null })
     }).catch(err => {
       console.log(err)
     })
